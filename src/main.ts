e498d285215316852a14b8976fa4dfa373ee071c
leobragaa <leombraga11@gmail.com>
2026-09-17 14:51:12 -0300

    ✨ Rota de login

--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,16 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module.js';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
-  console.log('http://localhost:3000/')
+  const app = await NestFactory.create(AppModule, { cors: true });
+  await app.listen(process.env.PORT ?? 3333);
+  console.log('http://localhost:3333/');
+
+  app.enableCors({
+    origin: 'http://localhost:3000/',
+    allowedHeaders:
+      'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept, Observe, Authorization',
+    methods: 'GET,PUT,POST,DELETE,PATCH',
+    credentials: true,
+  });
 }
 await bootstrap();
