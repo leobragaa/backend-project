34b23cdd209dfac7161007619916f2768e109b48
leobragaa <leombraga11@gmail.com>
2026-09-23 14:53:40 -0400

    ✨ Função de criação de usuario

--- src/usuario/entities/usuario.entity.ts
+++ src/usuario/entities/usuario.entity.ts
@@ -1,19 +1,28 @@
-import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
 
-@Entity('usuario', {schema: 'public', name:'usuario'})
+@Entity('usuario', { schema: 'public', name: 'usuario' })
 export class Usuario {
-    @PrimaryGeneratedColumn()
-    id: number
+  @PrimaryGeneratedColumn()
+  id: number;
 
-    @Column()
-    nome: string
+  @Column()
+  nome: string;
 
-    @Column()
-    senha: string
+  @Column()
+  senha: string;
 
-    @Column()
-    cfn: string
+  @Column()
+  cfn: string;
 
-    @Column()
-    email: string
+  @Column()
+  email: string;
+
+  @Column()
+  telefone: string;
+
+  @Column()
+  datanascimento: Date;
+
+  @Column()
+  tipousuario: string;
 }
