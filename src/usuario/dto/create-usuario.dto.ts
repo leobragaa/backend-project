76e53241a78246d6b20f1c2e063896da3af4c414
leobragaa <leombraga11@gmail.com>
2026-09-24 15:00:59 -0400

    🔥 Estruturando o Usuario

--- src/usuario/dto/create-usuario.dto.ts
+++ src/usuario/dto/create-usuario.dto.ts
@@ -1,9 +1,7 @@
 export class CreateUsuarioDto {
-  name: string;
+  nome: string;
   email: string;
   senha: string;
-  cfn: string;
   telefone: string;
-  datanascimento: string;
-  tipousuaro: string;
+  tipousuario: string;
 }
